6d855a1e35261897403f4df167c396a286407982
Guillermo Arellano <guillermo.arellanojr@gmail.com>
2024-07-03 13:56:00 -0500

    feat: add home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./feature/home/home.routes')
+  },
+  {
+    // fallback route (can be used to display dedicated 404 lazy feature)
+    path: '**',
+    redirectTo: ''
+  }
+];
---
+++ src/app/feature/home/home.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HomeComponent } from './home.component';
+import { provideExperimentalZonelessChangeDetection } from '@angular/core';
+
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HomeComponent],
+      providers: [provideExperimentalZonelessChangeDetection()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(HomeComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
