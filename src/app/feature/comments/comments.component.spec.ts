1242f74976715f26353def1054106c451e46293e
Guillermo Arellano <guillermo.arellanojr@gmail.com>
2024-07-03 22:28:55 -0500

    feat(user-mention): style dropdown
    add styles to view dropdown in separate lines

--- src/app/feature/comments/comments.component.spec.ts
+++ src/app/feature/comments/comments.component.spec.ts
@@ -20,4 +20,18 @@ describe('CommentsComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should not allow to mention same user more than once', undefined);
+
+  it('should close menu if ESC key pressed', undefined);
+
+  it('should notify valid users mentioned after @ character', undefined);
+
+  it('should bold valid users mentioned after @ character', undefined);
+
+  it('should allow keyboard up arrow to navigate in menu list', undefined);
+
+  it('should allow keyboard down arrow to navigate in menu list', undefined);
+
+  it('should allow keyboard enter key to make a selection in menu list', undefined);
 });
