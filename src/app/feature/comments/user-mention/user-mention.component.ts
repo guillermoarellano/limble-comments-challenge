747d2d301737656188ecec594d7d22d60898d7fb
Guillermo Arellano <guillermo.arellanojr@gmail.com>
2024-07-05 16:11:30 -0500

    feat(user-mention): keyboard navigation
    - be able to click up and down arrow the keyboard and not have cursor move inside the text box when menu open
    - be able to click on ESC key to close the menu
    - close menu when user clicks left or right key
    - first selected name in the list always received selection style when filtered.
    - tab key selects the user
    - enter key selects the user

--- src/app/feature/comments/user-mention/user-mention.component.ts
+++ src/app/feature/comments/user-mention/user-mention.component.ts
@@ -1,11 +1,10 @@
-import { CdkMenuModule } from '@angular/cdk/menu';
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'lmbl-user-mention',
   standalone: true,
-  imports: [CommonModule, CdkMenuModule], // TODO: get rid of common module, new v18 control flow
+  imports: [CommonModule], // TODO: get rid of common module, new v18 control flow
   templateUrl: './user-mention.component.html',
   styleUrl: './user-mention.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -22,13 +21,35 @@ export class UserMentionComponent {
 
   filteredUsers: { userID: number; name: string }[] = [];
   showMenu = false;
+  selectedIndex = -1;
   menuPosition = { x: 0, y: 0 };
 
-  onKeyUp(): void {
+  onKeyUp(keyupEvent: KeyboardEvent): void {
     const textarea = this.commentInput.nativeElement;
     const cursorPosition = textarea.selectionStart;
     const textBeforeCursor = textarea.value.substring(0, cursorPosition);
     const lastAtIndex = textBeforeCursor.lastIndexOf('@');
+
+    if ((this.showMenu && keyupEvent.key === 'ArrowDown') || keyupEvent.key === 'ArrowUp') {
+      return; // already handled by keydown arrow events
+    }
+
+    if (
+      this.showMenu &&
+      (keyupEvent.key === 'Escape' ||
+        keyupEvent.key === ' ' ||
+        keyupEvent.key === 'ArrowLeft' ||
+        keyupEvent.key === 'ArrowRight')
+    ) {
+      this.closeMenu();
+      return;
+    }
+
+    if (this.showMenu && (keyupEvent.key === 'Enter' || keyupEvent.key === 'Tab')) {
+      this.selectUser(this.filteredUsers[this.selectedIndex]);
+      this.closeMenu();
+      return;
+    }
 
     if (lastAtIndex !== -1) {
       const mentionQuery = textBeforeCursor.substring(lastAtIndex + 1);
@@ -36,17 +57,39 @@ export class UserMentionComponent {
 
       if (this.filteredUsers.length > 0) {
         this.showMenu = true;
+        this.selectedIndex = 0;
         const { top, left } = textarea.getBoundingClientRect();
         this.menuPosition = {
           x: left,
           y: top + cursorPosition * 1.2
         };
       } else {
-        this.showMenu = false;
+        this.closeMenu();
       }
     } else {
-      this.showMenu = false;
+      this.closeMenu();
     }
+  }
+
+  arrowUpDown(keyupEvent: Event): void {
+    if (this.showMenu) {
+      keyupEvent.preventDefault();
+      this.selectedIndex =
+        (this.selectedIndex + ((keyupEvent as KeyboardEvent).key === 'ArrowUp' ? -1 : 1) + this.filteredUsers.length) %
+        this.filteredUsers.length;
+    }
+  }
+
+  onTabDown(): void {
+    if (this.showMenu) {
+      this.selectUser(this.filteredUsers[this.selectedIndex]);
+      this.closeMenu();
+    }
+  }
+
+  closeMenu(): void {
+    this.showMenu = false;
+    this.selectedIndex = -1;
   }
 
   selectUser(user: { userID: number; name: string }): void {
@@ -58,7 +101,6 @@ export class UserMentionComponent {
     textarea.value =
       textBeforeCursor.substring(0, lastAtIndex + 1) + user.name + textarea.value.substring(cursorPosition);
 
-    this.showMenu = false;
-    textarea.focus();
+    this.closeMenu();
   }
 }
